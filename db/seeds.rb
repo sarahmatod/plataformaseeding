26a1c3e9445d910db8795fc76256f5033433f152
Sarah Matos <sarah.kubit@gmail.com>
2021-04-04 21:23:49 -0300

    collection seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@
 #
 # Examples:
 
-User.create(name: "equipeluizabarcelos", instagram: "luizabarcelos", email:"equipe@luizabarcelos.com", encrypted_password: "1234567890",  numer: 0, admin: true)
+User.create(name: "equipeluizabarcelos", instagram: "luizabarcelos", email: "equipe@luizabarcelos.com", encrypted_password: "1234567890",  numer: 0, admin: true)
+
 Collection.create(name: "inverno 2021", active: false)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
