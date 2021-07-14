1b763bd7eac295f60d3c525d25413a1083f96641
Sarah Matos <sarah.kubit@gmail.com>
2021-07-14 11:33:55 -0300

    email congif

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { host: "http://plataformalb-teste.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "https://plataformalb-teste.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
