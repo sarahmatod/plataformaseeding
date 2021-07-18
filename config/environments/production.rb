59abb108749cbff9e587e67f6945d1dda5340b14
Sarah Matos <sarah.kubit@gmail.com>
2021-07-18 11:36:46 -0300

    devise production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
 
-  config.action_mailer.default_url_options = { host: "https://plataformalb-teste.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "http://plataformalb-teste.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -112,4 +112,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            'some@gmail.com',
+  password:             'Not tellin ya!',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
+
 end
