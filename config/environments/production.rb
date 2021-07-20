b6cb08327bbc3318078889121629ac8e9b08025f
Sarah Matos <sarah.kubit@gmail.com>
2021-07-19 22:14:24 -0300

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,7 @@
 Rails.application.configure do
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password:ENV["GMAIL_PASSWORD"]
-  }
 
-
-  config.action_mailer.default_url_options = { :host => 'plataformalb-teste.herokuapp.com' }
+  #config.action_mailer.default_url_options = { :host => 'plataformalb-teste.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
