85f8d7875a1b50c03466cf40b3277ae488a1a4d5
Sarah Matos <sarah.kubit@gmail.com>
2021-07-20 10:52:52 -0300

    ddevise production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost', port: 3000 }
 
-config.action_mailer.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: "example.com",
