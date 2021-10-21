e8b45be7912724636606d70dbc4e39ee8f631031
Sarah Matos <sarah.kubit@gmail.com>
2021-10-20 22:13:14 -0300

    sem users index routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.all.each do |user|
-  user.delete
-end
+User.destroy_all
 
 User.create!(email: "admin@luizabarcelos.com.br", name: "equipe Luiza Barcelos", admin: true, password: "123123")
 
